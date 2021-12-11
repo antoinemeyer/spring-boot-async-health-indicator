837bae92efd779c51aa899fcbd8e8d6e7f38321f
antoine <antoine.meyer@trizic.com>
2021-12-11 11:40:23 -0500

    documentation simplification

--- src/main/java/com/teketik/spring/health/AsyncHealth.java
+++ src/main/java/com/teketik/spring/health/AsyncHealth.java
@@ -14,8 +14,8 @@ import java.util.concurrent.ThreadPoolExecutor;
  * <p>Makes a {@link HealthIndicator} {@link HealthIndicator#health() health method} periodically executed on a background {@link ThreadPoolExecutor}.<br>
  * <p>The {@link HealthIndicator#health()} method:
  * <ul>
- * <li>is executed with the given delay configured by {@link AsyncHealth#refreshRate() refreshRate()} (in second) between the termination of one execution and the commencement of the next.</li>
- * <li>may not execute in more than the time configured by {@link AsyncHealth#timeout() timeout()} (in second). Passed this delay, the thread running the {@link HealthIndicator} will be {@link Thread#interrupt() interrupted}
+ * <li>is executed with the given delay configured by {@link AsyncHealth#refreshRate() refreshRate()} between the termination of one execution and the commencement of the next.</li>
+ * <li>may not execute in more than the time configured by {@link AsyncHealth#timeout() timeout()}. Passed this delay, the thread running the {@link HealthIndicator} will be {@link Thread#interrupt() interrupted}
  * and the {@link Health} will be set as {@link Status#DOWN} until the next execution.</li>
  * </ul>
  * <p>The `/health` endpoint will not invoke the {@link HealthIndicator#health() health method} but return the last {@link Health} calculated asynchronously.
